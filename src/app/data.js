42e0b4dbcc08b02e7caedca1dd9eac752a30ac15
ezekwe chibuikem <ezekwechibuikem@gmail.com>
2025-10-21 14:51:42 +0100

    changed my cv

--- src/app/data.js
+++ src/app/data.js
@@ -1,92 +1,74 @@
-/*
-Websites:
-
-- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
-- https://lucide.dev/icons/ (Lucide Icons)
-- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
-- https://skillicons.dev (Skill Icons to show skills)
-- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
-
-:root {
-  --background: 27 27 27;
-  --foreground: 225 225 225;
-  --muted: 115 115 115;
-  --accent: 254 254 91; #FEFE5B
-}
-
-*/
-
 export const projectsData = [
   {
     id: 1,
-    name: "Eco Tracker",
-    description: "Track your carbon footprint",
+    name: "JAYS AND OAKS LTD",
+    description: "This is the official website of JAYS & OAKS Limited.",
     date: "2023-08-15",
-    demoLink: "https://ecotracker.example.com",
+    demoLink: "hhttps://www.jaysandoaks.com/",
   },
   {
     id: 2,
-    name: "Art Gallery Online",
-    description: "Digital art showcase platform",
-    date: "2023-06-20",
-    demoLink: "https://artgalleryonline.example.com",
+    name: "Chibuikem Portfolio",
+    description: "A personal developer portfolio showcasing my projects, technical skills, and professional journey in software development.",
+    date: "2022-06-20",
+    demoLink: "https://chibuikem.vercel.app/",
   },
   {
     id: 3,
-    name: "Budget Planner",
-    description: "Plan and track expenses",
-    date: "2023-09-10",
-    demoLink: "https://budgetplanner.example.com",
+    name: "Marriage Shapers",
+    description: "We believe every marriage can be reshaped into God’s original intention for love and unityPlan and track expenses",
+    date: "2025-08-10",
+    demoLink: "https://marriage-shapers.vercel.app",
   },
   {
     id: 4,
-    name: "Health Beat",
-    description: "Monitor heart rate zones",
+    name: "THRIVERIGHT",
+    description: "At THRIVERIGHT Health Foundation, we engage young minds through school outreaches to raise awareness about sickle cell disease",
     date: "2023-05-30",
-    demoLink: "https://healthbeat.example.com",
+    demoLink: "https://thriveright.netlify.app/",
   },
   {
     id: 5,
-    name: "Recipe Finder",
-    description: "Discover new recipes",
+    name: "THRIVERIGHT STAGGING",
+    description: "THF is a non-profit organization with the mission to elevate sickle cell awareness to its zenith across communities",
     date: "2023-07-12",
-    demoLink: "https://recipefinder.example.com",
+    demoLink: "https://thriveldt.netlify.app",
   },
-  {
-    id: 6,
-    name: "Journey Logger",
-    description: "Log your travels",
-    date: "2023-10-01",
-    demoLink: "https://journeylogger.example.com",
-  },
-  {
-    id: 7,
-    name: "Study Buddy",
-    description: "Collaborative learning platform",
-    date: "2023-04-18",
-    demoLink: "https://studybuddy.example.com",
-  },
-  {
-    id: 8,
-    name: "Tech Talk",
-    description: "Tech news aggregator",
-    date: "2023-11-05",
-    demoLink: "https://techtalk.example.com",
-  },
-  {
-    id: 9,
-    name: "Fit Track",
-    description: "Fitness and workout tracker",
-    date: "2023-03-22",
-    demoLink: "https://fittrack.example.com",
-  },
-  {
-    id: 10,
-    name: "Mindful Moments",
-    description: "Meditation and mindfulness app",
-    date: "2023-02-14",
-    demoLink: "https://mindfulmoments.example.com",
-  },
+  // {
+  //   id: 6,
+  //   name: "Journey Logger",
+  //   description: "Log your travels",
+  //   date: "2023-10-01",
+  //   demoLink: "https://journeylogger.example.com",
+  // },
+  // {
+  //   id: 7,
+  //   name: "Study Buddy",
+  //   description: "Collaborative learning platform",
+  //   date: "2023-04-18",
+  //   demoLink: "https://studybuddy.example.com",
+  // },
+  // {
+  //   id: 8,
+  //   name: "Tech Talk",
+  //   description: "Tech news aggregator",
+  //   date: "2023-11-05",
+  //   demoLink: "https://techtalk.example.com",
+  // },
+  // {
+  //   id: 9,
+  //   name: "Fit Track",
+  //   description: "Fitness and workout tracker",
+  //   date: "2023-03-22",
+  //   demoLink: "https://fittrack.example.com",
+  // },
+  // {
+  //   id: 10,
+  //   name: "Mindful Moments",
+  //   description: "Meditation and mindfulness app",
+  //   date: "2023-02-14",
+  //   demoLink: "https://mindfulmoments.example.com",
+  // },
 ];
 
 export const BtnList = [
@@ -108,13 +90,13 @@ export const BtnList = [
   },
   {
     label: "X",
-    link: "https://x.com/Bonaventex?t=TnalnttCHv06ukjfhG1wXA&s=09",
+    link: "https://x.com/3Djs_Dev",
     icon: "twitter",
     newTab: true,
   },
   {
     label: "Resume",
-    link: "/Ezekwe Chibuikem Bonaventure cv.pdf",
+    link: "/Ezekwe_Chibuikem_Bonaventure_cv.pdf",
     icon: "resume",
     newTab: true,
   },
